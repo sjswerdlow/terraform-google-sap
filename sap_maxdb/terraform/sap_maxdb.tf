e01e5376993b5ef86214d6a034317ad0ebde9bb6
Sam Swerdlow <sjswerdlow@google.com>
2021-07-28 14:57:38 -0400

    First attempt at making the terraform template for the SAP MaxDB. Disks and VM were spun up while developing locally. Next steps are to update the build.sh file and to develop from a bucket.
    Change-Id: I5732b2d3653f80605acdf9c5629a1a0fbb2ecff7

---
+++ sap_maxdb/terraform/sap_maxdb.tf
@@ -0,0 +1,52 @@
+
+#
+# Version:    BUILD.VERSION
+# Build Hash: BUILD.HASH
+#
+module "maxdb" {
+  source = "TERRAFORM_PREFIXTERRAFORM_URL/dm-templates/sap_maxdb/sap_maxdb_module.zip"
+  #
+  # By default, this source file uses the latest release of the terraform module
+  # for SAP on Google Cloud.  To fix your deployments to a specific release
+  # of the module, comment out the source property above and uncomment the source property below.
+  #
+  # source = "TERRAFORM_PREFIXTERRAFORM_URL/dm-templates/sap_maxdb/sap_maxdb_module.zip"
+  #
+  # Fill in the information below
+  #
+  ### Sam these are all the default values from variables.tf.
+  instance_name              = "MV_NAME"              # example: hana_scaleout
+  machine_type               = "MACHINE_TYPE"         # example: n1-highmem-32
+  project_id                 = "PROJECT_ID"           # example: customer-project-x
+  zone                       = "ZONE"                 # example: us-central1-a
+  subnetwork                 = "SUBNETWORK"           # example: default
+  linux_image                = "IMAGE_FAMILY"         # example: sles-15-sp2-sap
+  linux_image_project        = "IMAGE_PROJECT"        # example: suse-sap-cloud
+  maxdb_sid                  = "MAXDB_DATABASE_SID"   # example: SID
+
+  # Optional disk configuration
+
+  # maxdb_root_size            = ROOT_DISK_SIZE # in GB, default is 8
+  # maxdb_data_size            = DATA_DISK_SIZE # in GB, default is 30
+  # maxdb_log_size             = LOG_DISK_SIZE  # in GB, default is 8
+  # maxdb_data_ssd             = TRUE_OR_FALSE       # default is true
+  # maxdb_log_ssd              = TRUE_OR_FALSE       # default is true
+
+  # Optional advanced options
+
+  # maxdb_backup_size         = BACKUP_DISK_SIZE  # in GB, default is 0 and will not be created
+  # usrsap_size               = USR_DISK_SIZE # Required if SAP NetWeaver is on same VM in GB, default is 0 and will not be created
+  # sapmnt_size               = MNT_DISK_SIZE # Required if SAP NetWeaver is on same VM in GB, default is 0 and will not be created
+  # swap_size                 = SWAP_DISK_SIZE # Required if SAP NetWeaver is on same VM in GB, default is 0 and will not be created
+
+  # Optional advanced options
+  # network_tags           = ["TAG"]
+  # public_ip              = TRUE_OR_FALSE            # default is true
+  # service_account        = "CUSTOM_SERVICE_ACCOUNT"
+  # sap_deployment_debug   = TRUE_OR_FALSE            # default is false
+  # use_reservation_name   = "RESERVATION_NAME"
+
+  # Developer options
+  # primary_startup_url    = "SCRIPT_URL"
+  # post_deployment_script = "SCRIPT_URL"
+}
