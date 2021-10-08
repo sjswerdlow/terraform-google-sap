b8793663a09954ff421d26445bfeb98eabc185d4
Sam Swerdlow <sjswerdlow@google.com>
2021-10-08 17:30:29 +0000

    Hotfixing a name for ASE mount. See: https://cloud.google.com/solutions/sap/docs/sap-ase-deployment-linux#verifying_deployment
    Change-Id: I838e9fb0bc87f6b81e6cfe02433ef8527c7dd348

--- lib/sap_lib_ase.sh
+++ lib/sap_lib_ase.sh
@@ -3,7 +3,7 @@ ase::create_filesystems() {
   main::errhandle_log_info "Creating file systems for SAP ASE"
   main::create_filesystem /sybase/"${VM_METADATA[sap_ase_sid]}" asesid xfs
   main::create_filesystem /sybase/"${VM_METADATA[sap_ase_sid]}"/sapdata_1 asesapdata xfs
-  main::create_filesystem /sybase/"${VM_METADATA[sap_ase_sid]}"/loglog_1 aselog xfs
+  main::create_filesystem /sybase/"${VM_METADATA[sap_ase_sid]}"/saplog_1 aselog xfs
   main::create_filesystem /sybase/"${VM_METADATA[sap_ase_sid]}"/saptemp asesaptemp xfs
   main::create_filesystem /sybase/"${VM_METADATA[sap_ase_sid]}"/sapdiag asesapdiag xfs
   main::create_filesystem /sybasebackup asebackup xfs
