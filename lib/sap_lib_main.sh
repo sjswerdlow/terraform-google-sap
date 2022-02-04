720bd0f673e6178a46b2efdffb2817cb8539d8e6
Sam Swerdlow <sjswerdlow@google.com>
2022-02-04 23:12:34 +0000

    Updating the lib to include the necessary Python3 libs.
    Change-Id: Id8cacee5b8837c633f6680eb5d6f7464a4c3c969

--- lib/sap_lib_main.sh
+++ lib/sap_lib_main.sh
@@ -130,7 +130,7 @@ main::install_packages() {
   fi
 
   ## packages to install
-  local sles_packages="libssh2-1 libopenssl0_9_8 libopenssl1_0_0 joe tuned krb5-32bit unrar SAPHanaSR SAPHanaSR-doc pacemaker numactl csh python-pip python-pyasn1-modules ndctl python-oauth2client python-oauth2client-gce python-httplib2 python-requests python-google-api-python-client libgcc_s1 libstdc++6 libatomic1"
+  local sles_packages="libssh2-1 libopenssl0_9_8 libopenssl1_0_0 joe tuned krb5-32bit unrar SAPHanaSR SAPHanaSR-doc pacemaker numactl csh python-pip python-pyasn1-modules ndctl python-oauth2client python-oauth2client-gce python-httplib2 python3-httplib2 python3-google-api-python-client python-requests python-google-api-python-client libgcc_s1 libstdc++6 libatomic1"
   local rhel_packages="unar.x86_64 tuned-profiles-sap-hana tuned-profiles-sap-hana-2.7.1-3.el7_3.3 joe resource-agents-sap-hana.x86_64 compat-sap-c++-6 numactl-libs.x86_64 libtool-ltdl.x86_64 nfs-utils.x86_64 pacemaker pcs lvm2.x86_64 compat-sap-c++-5.x86_64 csh autofs ndctl compat-sap-c++-9 libatomic unzip libsss_autofs python2-pip langpacks-en langpacks-de glibc-all-langpacks libnsl libssh2 wget lsof"
 
   ## install packages
