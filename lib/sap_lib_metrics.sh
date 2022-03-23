68d3f2aea3e92fea49846f7d12092c8c0e38c398
Sam Swerdlow <sjswerdlow@google.com>
2022-03-23 17:21:07 +0000

    Temporarily taking out the usage metrics from Accelerator Templates.
    Removed the usage metrics due to turbo launch conflict concerns. Newer version of usage metrics lib to be added back in at a future date.
    
    BUG: 219986242
    
    Change-Id: Icf766c2e05a71386866dfc1f8a0498a014c4580f

--- lib/sap_lib_metrics.sh
+++ lib/sap_lib_metrics.sh
@@ -2,69 +2,7 @@
 
 # send_metrics should generally be called from a sub-shell. It should never exit the main process.
 metrics::send_metric() {(  #Exits will only exit the sub-shell.
-    local SKIP_LOG_DENY_LIST=("510599941441" "1038306394601" "714149369409" "161716815775" "607888266690" "863817768072" "450711760461" "600915385160" "114837167255" "39979408140" "155261204042" "922508251869" "208472317671" "824757391322" "977154783768" "148036532291" "425380551487" "811811474621" "975534532604" "475132212764" "201338458013" "269972924358" "400774613146" "977154783768" "425380551487" "783555621715" "182593831895" "1042063780714" "1001412328766" "148036532291" "135217527788" "444363138560" "116074023633" "545763614633" "528626677366" "871521991065" "271532348354" "706203752296" "742377328177" "756002114100" "599169460194" "880648352583" "973107100758" "783641913733" "355955620782" "653441306135" "703965468432" "381292615623")
-
-    local NUMERIC_VM_PROJECT=$(main::get_metadata "http://169.254.169.254/computeMetadata/v1/project/numeric-project-id")
-    local VM_IMAGE_FULL=$(main::get_metadata "http://169.254.169.254/computeMetadata/v1/instance/image")
-    local VM_ZONE=$(main::get_metadata "http://169.254.169.254/computeMetadata/v1/instance/zone" | cut -d / -f 4 )
-    local VM_NAME=$(main::get_metadata "http://169.254.169.254/computeMetadata/v1/instance/name")
-    local METADATA_URL="https://compute.googleapis.com/compute/v1/projects/${VM_PROJECT}/zones/${VM_ZONE}/instances/${VM_NAME}"
-
-    while getopts 's:n:v:e:u:c:p:' argv; do
-        case "${argv}" in
-        s) status="${OPTARG}";;
-        e) error_id="${OPTARG}";;
-        u) updated_version="${OPTARG}";;
-        c) custom_data="${OPTARG}";;
-        esac
-    done
-
-    if [[ -z "${VM_METADATA[template-type]}" ]]; then
-        VM_METADATA[template-type]="UNKNOWN"
-    fi
-    if [[ -z "${TEMPLATE_NAME}" ]]; then
-        TEMPLATE_NAME="UNSET"
-    fi
-
-    metrics::validate "${status}" "Missing required status (-s) argument."
-    # We don't want to log our own test runs:
-    if [[ " ${SKIP_LOG_DENY_LIST[*]} " == *" ${NUMERIC_VM_PROJECT} "* ]]; then
-        echo "Not logging metrics this is an internal project."
-        exit 2
-    fi
-    if [[ $VM_IMAGE_FULL =~ ^projects/(centos-cloud|cos-cloud|debian-cloud|fedora-coreos-cloud|rhel-cloud|rhel-sap-cloud|suse-cloud|suse-sap-cloud|ubuntu-os-cloud|ubuntu-os-pro-cloud|windows-cloud|windows-sql-cloud)/global/images/.+$ ]]; then
-        VM_IMAGE=$(echo "${VM_IMAGE_FULL}" | cut -d / -f 5)
-    else
-        VM_IMAGE="unknown"
-    fi
-
-    local template_id="${VM_METADATA[template-type]}-${TEMPLATE_NAME}"
-    case $status in
-    RUNNING|STARTED|STOPPED|CONFIGURED|MISCONFIGURED|INSTALLED|UNINSTALLED)
-        user_agent="sap-core-eng/accelerator-template/${BUILD.VERSION}/${VM_IMAGE}/${status}"
-        ;;
-    ERROR)
-        metrics::validate "${error_id}" "'ERROR' statuses require the error message (-e) argument."
-        user_agent="sap-core-eng/accelerator-template/${BUILD.VERSION}/${VM_IMAGE}/${status}/${error_id}-${template_id}"
-        ;;
-    UPDATED)
-        metrics::validate "${updated_version}" "'UPDATED' statuses require the updated version (-u) argument."
-        user_agent="sap-core-eng/accelerator-template/${BUILD.VERSION}/${VM_IMAGE}/${status}/${updated_version}"
-        ;;
-    CUSTOM)
-        metrics::validate "${custom_data}" "'CUSTOM' statuses require the custom data (-c) argument."
-        user_agent="sap-core-eng/accelerator-template/${BUILD.VERSION}/${VM_IMAGE}/${status}/${custom_data}"
-        ;;
-    TEMPLATEID)
-        user_agent="sap-core-eng/accelerator-template/${BUILD.VERSION}/${VM_IMAGE}/CUSTOM/${template_id}"
-        ;;
-    *)
-        echo "Error, valid status must be provided."
-        exit 2
-    esac
-
-    local curlToken=$(metrics::get_token)
-    curl --fail -H "Authorization: Bearer ${curlToken}" -A "${user_agent}" "${METADATA_URL}"
+    return 0
 )}
 
 
