9f7c184f1bfdbd428e35ce57ff24ae677376d139
Sam Swerdlow <sjswerdlow@google.com>
2022-06-06 14:28:47 +0000

    Hotfix for a validation bug in the nw_ha space.
    Change-Id: Ifd2a0c469da21f8827fccb0e99033912924e2bc9

--- sap_nw_ha/terraform/sap_nw_ha/variables.tf
+++ sap_nw_ha/terraform/sap_nw_ha/variables.tf
@@ -72,7 +72,7 @@ variable "sap_sid" {
   type = string
   description = "SAP System ID"
   validation {
-    condition = length(var.sap_hana_sid) == 3 && can(regex("[A-Z][A-Z0-9]{2}", var.sap_hana_sid))
+    condition = length(var.sap_sid) == 3 && can(regex("[A-Z][A-Z0-9]{2}", var.sap_sid))
     error_message = "The sap_sid must be 3 characters long and start with a letter and all letters must be capitalized."
   }
 }
